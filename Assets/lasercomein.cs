93b772b623076fcee765839bbfbbea48eb52571e
Benny <76574420+bennytrieu@users.noreply.github.com>
2023-02-27 21:04:48 -1000

    laser almost done

--- Assets/lasercomein.cs
+++ Assets/lasercomein.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class lasercomein : MonoBehaviour
 {
     public GameObject player;
+
+    // public bool record = false;
+    public GameObject spawnercheck;
     [SerializeField] GameObject laser;
+
+    float laserYPosition;
+    spawner spawnerscript;
 
     laserflash comein;
     // Start is called before the first frame update
+    
     void Start()
     {
         player = GameObject.Find("Templayer");
+        spawnercheck = GameObject.Find("Spawner");
+        spawnerscript = spawnercheck.GetComponent<spawner>();
     }
 
 
@@ -22,10 +31,13 @@ public class lasercomein : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + new Vector3(-26, 0, 0);
+        // originally it was -26, 0, 0
+        laserYPosition = spawnercheck.transform.position.y;
+        transform.position = player.transform.position + new Vector3(-26, laserYPosition, 0);
         if (comein.destroyit == true)
         {
             Destroy(gameObject);
+            spawnerscript.isSpawned = false;
         }
     }
 
