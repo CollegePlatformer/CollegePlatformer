2268e31003dfb3e52ffccb63c3fff5cba16bfa49
John Suelen <jsuelen@hawaii.edu>
2023-03-06 18:09:43 -1000

    Organized hierarchy and files

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,18 +7,22 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float walkSpeed = 10.0f;
     [SerializeField] float jump = 5.0f;
     [SerializeField] float gravityModifier = 1.0f;
+    [SerializeField]
+    private float turnSpeed = 5f;
     private float horizontalInput;
     private int jumpCount = 2;
     public bool atFinish = false;
     public bool isDead = false;
     private bool isOnGround = true;
     private Rigidbody playerRb;
+    private Animator studentAnim;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
+        studentAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,11 +30,22 @@ public class PlayerController : MonoBehaviour
     {
         Debug.Log(jumpCount);
         playerMovement();
+
+        if(horizontalInput != 0)
+        {
+            studentAnim.SetBool("Run_Anim", true);
+        }
+        else
+        {
+            studentAnim.SetBool("Run_Anim", false);
+        }
+
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             Debug.Log("jump input pressed");
             //Debug.Log(jumpCount);
-
+            studentAnim.SetBool("Jump_Anim", true);
+            studentAnim.SetBool("Run-Jump", true);
             playerRb.AddForce(Vector3.up * jump, ForceMode.Impulse);
             isOnGround = false;
             jumpCount--;
@@ -48,7 +63,17 @@ public class PlayerController : MonoBehaviour
     void playerMovement()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * walkSpeed);
+        Vector3 direction = new Vector3(horizontalInput, 0, 0);
+        direction.Normalize();
+        
+        transform.Translate(direction * walkSpeed * Time.deltaTime, Space.World);
+
+        if(direction != Vector3.zero)
+        {
+            //transform.forward = direction;
+            Vector3 toRotation = Vector3.RotateTowards(transform.forward, direction, Time.deltaTime * turnSpeed, 0.0f);
+            transform.rotation = Quaternion.LookRotation(direction);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -56,6 +81,8 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Platform"))
         {
             isOnGround = true;
+            studentAnim.SetBool("Jump_Anim", false);
+            studentAnim.SetBool("Run-Jump", false);
             jumpCount = 2;
             Debug.Log(collision.gameObject.tag);
         }
