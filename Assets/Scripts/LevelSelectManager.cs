66bc17314a4ff1a39f6ebeac73fd1c95cf9e100c
John Suelen <jsuelen@hawaii.edu>
2023-04-02 19:38:35 -1000

    basic level selector

---
+++ Assets/Scripts/LevelSelectManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectManager : MonoBehaviour
+{
+    // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    public void Level1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
+
+    public void Level5()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 5);
+    }
+}
